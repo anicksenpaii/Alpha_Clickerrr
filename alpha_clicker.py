84a29663a68f9a6777408a1907e6587708ca2801
Abhimanyu Singh <abhimanyusingh.2k6@gmail.com>
2025-09-13 21:19:10 +0530

    Update alpha_clicker.py

--- alpha_clicker.py
+++ alpha_clicker.py
@@ -1,8 +1,58 @@
 #!/usr/bin/env python3
+# import sys
+# import subprocess
+# import time
+# from pymavlink import mavutil
+
+# try:
+#     master = mavutil.mavlink_connection('/dev/ttyACM0', baud=115200)
+#     master.wait_heartbeat()
+#     print("succesfully connected to vehicle")
+# except Exception as e:
+#     print(f"Failed to connect to vehicle: {e}")
+#     sys.exit(1)
+
+# master.mav.request_data_stream_send(
+#     master.target_system,
+#     master.target_component,
+#     mavutil.mavlink.MAV_DATA_STREAM_RC_CHANNELS,
+#     1,1)
+    
+# button_pressed = False
+# subprocess.run([
+#     "sudo","modprobe","v4l2loopback",
+#     "devices=1", "video_nr=0", 'card_label="SonyAlpha"', "exclusive_caps=1"
+#     ])
+# counter = 0
+# while True:
+#     msg = master.recv_match(type=['RC_CHANNELS_RAW','RC_CHANNELS'], blocking=True, timeout=1)
+#     if not msg:
+#         continue
+#     rc_channel = msg.chan9_raw
+
+#     if rc_channel > 1500 and not button_pressed:
+#         button_pressed = True
+
+#     if rc_channel < 1500:
+#         button_pressed = False
+
+#     if button_pressed == True:
+#         filename = f"image{counter}.jpg"
+#         subprocess.run([
+#             "gphoto2", "--capture-image-and-download", "--filename", filename
+#         ])
+#         counter += 1
+#     time.sleep(0.5)
+
+#!/usr/bin/env python3
 import sys
 import subprocess
 import time
 from pymavlink import mavutil
+
+remote_host_username = 'xyz'
+remote_host_ip = '192.168.0.0.1'
+remote_path = '/home/xyz/images'
 
 try:
     master = mavutil.mavlink_connection('/dev/ttyACM0', baud=115200)
@@ -42,4 +92,8 @@ while True:
             "gphoto2", "--capture-image-and-download", "--filename", filename
         ])
         counter += 1
+        subprocess.run([
+            "scp", filename, f"{remote_host_username}@{remote_host_ip}:{remote_path}"
+        ], check=True)
+        print(f"✅ Sent {filename} to {remote_path}")
     time.sleep(0.5)
